a265c41abf0ca8436f859639cf93b049d3705047
naimad96 <damian.kursa969@gmail.com>
2018-07-13 23:01:46 +0200

    'fourth'

--- js/jquery.js
+++ js/jquery.js
@@ -1,8 +1,22 @@
 let menuIcon = $('.menu-icon');
-
+let scroll = window.scrollTop;
+//Menu icon animation
 menuIcon.on('click',function(){
-  $(".navigation-list-hidden").animate({ width: 'toggle' },200).attr('class','navigation-list');
+  $(".navigation-list-hidden").slideDown(600)/*.animate({width:'toggle'},600)*/.attr('class','navigation-list');
 });
 $('.close-icon').on('click',function(){
-  $('.navigation-list').animate({ width: 'toggle' },200).attr('class','navigation-list-hidden');
+  $('.navigation-list').animate({width:'toggle'},600);
+  setTimeout(
+    function(){
+      $('.navigation-list').attr('class','navigation-list-hidden');
+    },1100);
 });
+ $(window).scroll(function(){
+   let scrollV = $(window).scrollTop();
+   if(scrollV > 100){
+         $('.logo-container').attr('class','logo-container-opacity');
+       }else{
+         $('.logo-container-opacity').attr('class','logo-container');
+
+       }
+ });
