8eb457fc76f0211085b9872c1b6195cb56967378
naimad96 <damian.kursa969@gmail.com>
2018-07-13 18:51:50 +0200

    'change'

--- js/jquery.js
+++
@@ -1,6 +0,0 @@
-let signupBtn = $('.signup');
-let loginBtn = $('.login');
-let loginForm = $('.login-form-hidden');
-loginBtn.on('click', function(){
-  loginForm.animate({height:'toggle'}).attr('class','login-form');
-});
